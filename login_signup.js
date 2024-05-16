7d2276b31731e68e745a0da095bb92425ba16e59
Abdelbar Amejjoud <abbelabbel9@gmail.com>
2024-05-16 23:30:42 +0100

    Update login_signup.js

--- login_signup.js
+++ login_signup.js
@@ -1,67 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const tabList = document.querySelector(".tabs");
+		let login = document.querySelector('.login');
+		let create = document.querySelector('.create');
+		let container = document.querySelector('.container');
 
-  tabList.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
-
-    const tabIndex = e.target.dataset.tab;
-    if (!tabIndex) return;
-
-    const tabs = document.querySelectorAll("[data-tab]");
-    tabs.forEach((tab) => {
-      if (tab.dataset.tab === tabIndex) {
-        tab.classList.add("active");
-      } else {
-        tab.classList.remove("active");
-      }
-    });
-
-    const activePanel = document.querySelector(".panel.active");
-    const toActivePanel = document.querySelector(`[data-panel="${tabIndex}"]`);
-    activePanel.classList.add("close");
-    activePanel.addEventListener(
-      "animationend",
-      () => {
-        activePanel.classList.remove("active");
-        activePanel.classList.remove("close");
-        toActivePanel.classList.add("active");
-      },
-      { once: true }
-    );
-  });
-
-  const showHidePasswords = (type) => {
-    const passwordButton = document.getElementById(`${type}-password-eye`);
-    passwordButton.addEventListener("click", (e) => {
-      const passwordInput = document.getElementById(`${type}-password`);
-      const icon = passwordButton.querySelector("i");
-      const isVisible = icon.classList.contains("ri-eye-fill");
-      passwordInput.type = isVisible ? "password" : "text";
-      icon.setAttribute("class", isVisible ? "ri-eye-off-fill" : "ri-eye-fill");
-    });
-  };
-
-  showHidePasswords("login");
-  showHidePasswords("signup");
-  showHidePasswords("confirm");
-
-  // Add event listener for login button
-  const loginButton = document.querySelector('[data-tab="1"] button');
-  loginButton.addEventListener("click", function(event) {
-    event.preventDefault(); 
-
-
-    window.location.href = "page_x.html";
-  });
-
-
-  const signUpButton = document.querySelector('[data-tab="2"] button');
-  signUpButton.addEventListener("click", function(event) {
-
-    const loginTabButton = document.querySelector('[data-tab="1"] button');
-    loginTabButton.click(); 
-
-
-    alert("Thank you for signing up!");
-  });
-});
+		login.onclick = function(){
+			container.classList.add('signinForm');
+		}
+		
+		create.onclick = function(){
+			container.classList.remove('signinForm');
+		}
